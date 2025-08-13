8c72f84d73fa8464d4be56671caa2c59eb1f694a
Nath Lobats <n.e.m.q.v.5@gmail.com>
2025-08-12 22:15:23 -0300

    Documenta acesso ao portal em portugues

---
+++ app.py
@@ -0,0 +1,15 @@
+import http.server
+import socketserver
+
+PORT = 8000
+DIRECTORY = "templates"
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+
+if __name__ == "__main__":
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print(f"Serving at port {PORT}")
+        httpd.serve_forever()
